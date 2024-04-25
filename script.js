b928f43bf43dbb522d2af066211867fa65bae827
Sisti1 <shristigoyal516@gmail.com>
2024-04-25 20:18:07 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+const toggleBtns = document.querySelectorAll('.toggle-btn');
+
+toggleBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // Remove active class from all buttons
+    toggleBtns.forEach(btn => btn.classList.remove('active'));
+    // Add active class to the clicked button
+    btn.classList.add('active');
+  });
+});
